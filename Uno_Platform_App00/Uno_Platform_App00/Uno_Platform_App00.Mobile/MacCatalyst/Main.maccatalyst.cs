b72d2212e6e28ae7e236dae55f1395f904d57acf
Shinya.Umeda <umeda@barnet.co.jp>
2022-09-08 14:45:36 +0900

    Uno platform app ソリューション作り立て

---
+++ Uno_Platform_App00/Uno_Platform_App00/Uno_Platform_App00.Mobile/MacCatalyst/Main.maccatalyst.cs
@@ -0,0 +1,15 @@
+﻿using UIKit;
+
+namespace Uno_Platform_App00
+{
+    public class EntryPoint
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, typeof(App));
+        }
+    }
+}
